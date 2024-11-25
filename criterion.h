c564ec90b1731f70abc6ffd238f451af0f0b469e
ihosny <ihosny@uwaterloo.ca>
2024-11-25 16:01:05 -0500

    added ctor

--- criterion.h
+++ criterion.h
@@ -7,16 +7,20 @@
 
 class Criterion: public Achievement {
   public:
-    enum class CompletionLevel {ASSIGNMENT = 1, MIDTERM, EXAM};
-
-    void update();
-
-    CompletionLevel getCompletion(); // getter
-
-    ~Criterion();
+    enum class CompletionLevel {INCOMPLETE = 0, ASSIGNMENT, MIDTERM, EXAM};
 
   private:
     CompletionLevel completion;
+
+  public:
+    Criterion(int id, std::map<Resource, int> cost, std::shared_ptr<Student> owner = nullptr, CompletionLevel completion = CompletionLevel::INCOMPLETE);
+
+    void update();
+
+    CompletionLevel getCompletion() const; // getter
+
+    ~Criterion() override;
+
 };
 
 #endif
